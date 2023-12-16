bd58ac0ddf1cf615138011b3d78e8a74201f8d30
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2023-12-15 22:23:25 -0600

    Metadata - Metatags

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,12 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Miguel's Contact Page",
+    description: 'Generated with love by Vercel',
+    keywords: ['Contact Page', 'Miguel', 'Información', ]
+  }
+  
+
 export default function ContactPage() {
     return (
         <>
--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,3 +1,12 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Miguel's Pricing Page",
+    description: 'Generated with love by Vercel',
+    keywords: ['Pricing Page', 'Miguel', 'Información', ]
+  }
+  
+
 export default function PricingPage() {
     return (
         <>
